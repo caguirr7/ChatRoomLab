610af28d244f463bfdef2c3f2ec959bad32c980a
Chris Aguirre <caguirr7@montgomerycollege.edu>
2021-05-07 20:38:24 -0400

    Changed redundancy on the Thread class

--- src/ChatRoomLab/ChatServerExec.java
+++ src/ChatRoomLab/ChatServerExec.java
@@ -28,12 +28,11 @@ public class ChatServerExec {
     private static int CHAT_ROOM_PORT;
     /**
      * Starts an instance of a server in a thread so that GUI thread can continue to operate asynchronously
-     * @param port
      */
    public void startServer() {
     	ChatServer server = new ChatServer(CHAT_ROOM_PORT);
     	//TODO STUDENT: start a thread with server in it
-       Thread thread = new Thread ((Runnable) server);
+       Thread thread = new Thread (server);
        thread.start();
     }
 }
