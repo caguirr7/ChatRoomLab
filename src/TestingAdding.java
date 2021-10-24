56c343da6d4a76a9d2e134410d28fa9ba6a00d06
noobmaster69 <cjaguirre1988@gmail.com>
2021-10-24 02:21:37 -0400

    push chat room

--- src/TestingAdding.java
+++ src/TestingAdding.java
@@ -2,5 +2,7 @@ public class TestingAdding {
     public static void main(String[] args){
 
         System.out.println("Adding from another computer");
+
+        System.out.println("Adding a comment from the Lenovo");
     }
 }
